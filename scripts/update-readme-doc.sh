d19c182c73f50dec5b42f1e5a09a3cb0f6f83e96
Oleg Balunenko <oleg.balunenko@gmail.com>
2023-03-26 13:13:03 +0400

    chore: Add helper for generating readme doc

---
+++ scripts/update-readme-doc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_DIR="$(dirname "$0")"
+REPO_ROOT="$(cd "${SCRIPT_DIR}" && git rev-parse --show-toplevel)"
+SCRIPTS_DIR="${REPO_ROOT}/scripts"
+
+source "${SCRIPTS_DIR}/helpers-source.sh"
+
+echo "${SCRIPT_NAME} is running... "
+
+checkInstalled 'goreadme'
+
+
+goreadme -credit=false -skip-sub-packages  > README_doc.md
+
+echo "${SCRIPT_NAME} done."
